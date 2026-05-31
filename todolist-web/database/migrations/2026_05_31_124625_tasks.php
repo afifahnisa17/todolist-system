dbcc754c6385d4aac1d8a2ec2126320f3d86b5a9
afifahnisa17 <afifahnisa041@gmail.com>
2026-05-31 20:48:53 +0700

    feat: make authentication using laravel breeze

--- todolist-web/database/migrations/2026_05_31_124625_tasks.php
+++ todolist-web/database/migrations/2026_05_31_124625_tasks.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('description')->nullable();
             $table->timestamp('task_date')->nullable();
-            $table->boolean('status')->default(false);
+            $table->enum('status', ['pending', 'in_progress', 'completed'])->default('pending');
             $table->timestamps();
         });
     }
